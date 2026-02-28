bc4f8b7e11081fb46e6c346eb904f58b3e0d71de
Damar <damar@example.com>
2026-02-28 10:28:50 +0700

    tambahkan sedikit detail

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AboutController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -10,6 +12,8 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/about', [AboutController::class, 'index'])->middleware(['auth', 'verified'])->name('about');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
